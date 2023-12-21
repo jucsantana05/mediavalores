d2d385d2b4a197e09b83140e07e60e6220fbc442
jucsantana05 <139167703+jucsantana05@users.noreply.github.com>
2023-12-21 16:29:18 -0300

    Add files via upload

---
+++ media/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Informe os valores desejados: ");
+        int quantidadevalor = int.Parse(Console.ReadLine());
+
+        var listadevalores = ColetarValores(quantidadevalor);
+
+        Console.WriteLine("A soma dos valores é: " + listadevalores);
+        Console.WriteLine("A média dos valores é: " + listadevalores);
+
+        Console.ReadLine();
+
+    }
+    static List<decimal> ColetarValores(int quantidadevalor)
+    {
+        List<decimal> listadevalores = new List<decimal>(quantidadevalor);
+
+        for (int i = 0; i < quantidadevalor; i++)
+        {
+            Console.WriteLine($"Informe o valor + {i + 1}");
+            listadevalores.Add(decimal.Parse(Console.ReadLine()));
+        }
+
+
+        return listadevalores;
+    }
+
+    static decimal CalcularSoma(List<decimal> listadevalores)
+    {
+        decimal total = 0;
+        foreach (decimal valor in listadevalores)
+            total += valor;
+
+        return total;
+    }
+
+    static decimal CalcularMedia(List<decimal> listadevalores)
+    {
+        decimal soma = CalcularSoma(listadevalores);
+        decimal media = soma / listadevalores.Count;
+        return media;
+
+        
+    }
+
+}
